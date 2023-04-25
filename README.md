49389ea6b9af7a9455d2d2bd308d052b564d2746
youngju-jang <joj1043@nate.com>
2023-04-25 23:15:32 +0900

    [Feat] 4.1 Making Our Routers
    related to #12, #10

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ## Wetube Clone build using NodeJs, Express, Mongo and ES6 ❤️
+
+### global router
+/ : home
+/join : Join
+/login : Login
+/search : Search
+
+### user router
+/users/edit : Edit uer
+/users/delete : Delete user
+
+### video router
+/videos/watch : Watch Video
+/videos/edit : Edit Video
+/videos/delete : Delete Video
+/videos/comments : Comment on a video
+/videos/comments/delete : delete A comment of a video
--- src/server.js
+++ src/server.js
@@ -2,32 +2,31 @@ import express from "express";
 import morgan from "morgan";
 
 const PORT = 4000;
+
 const app = express(); // express application 생성
-const logger = morgan("combined");
-// const logger = (req, res, next) => {
-//     console.log(`${req.method} ${req.url}`); // GET /
-//     next();
-// }
-const privateMiddleware = (req, res, next)=> {
-    const url = req.url; 
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue");
-    next();
-}
-const handleHome = (req, res) => {
-    return res.end(" I love middlewares ");
-}
-
-const handleProtected = (req, res) =>{
-    return res.send("Welcome to the private lounge");
-}
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome); // root 로 get request를 보내면 콜백함수를 실행함
-app.get("/protected", handleProtected);
+const globalRouter = express.Router(); // /~
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router(); // /users/~
+
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router(); // /videos/~
+
+const handleWatchVideo = (req, res) => res.send("watch video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}🌈`);
 
 app.listen(PORT, handleListening); // 몇번 port
